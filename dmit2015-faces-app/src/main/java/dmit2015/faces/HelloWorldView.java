6211ae3ac1b55f5b3c91a36d8744fe10db035281
Sam Wu <swu@nait.ca>
2025-05-14 20:50:39 -0600

    Added Hello World with Input example

---
+++ dmit2015-faces-app/src/main/java/dmit2015/faces/HelloWorldView.java
@@ -0,0 +1,62 @@
+package dmit2015.faces;
+
+import jakarta.inject.Inject;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+import lombok.Setter;
+import org.omnifaces.util.Faces;
+import org.omnifaces.util.Messages;
+
+import jakarta.faces.view.ViewScoped;
+import jakarta.inject.Named;
+import jakarta.annotation.PostConstruct;
+
+import java.io.Serializable;
+
+@Named("currentHelloWorldView")
+@ViewScoped // create this object for one HTTP request and keep in memory if the next is for the same page
+// class must implement Serializable
+public class HelloWorldView implements Serializable {
+
+    // Declare read/write properties (field + getter + setter) for each form field
+    @Getter @Setter
+    @NotBlank(message = "Please enter a username value.")
+    private String username;
+
+    @PostConstruct // This method is executed after DI is completed (fields with @Inject now have values)
+    public void init() { // Use this method to initialized fields instead of a constructor
+        // Code to access fields annotated with @Inject
+
+    }
+
+    public void onSubmit() {
+        try {
+            Messages.addGlobalInfo("Hello {0} and welcome Faces World!", username);
+
+        } catch (Exception ex) {
+            handleException(ex);
+        }
+    }
+
+    public void onClear() {
+        username = null;
+    }
+
+    /**
+     * This method is used to handle exceptions and display root cause to user.
+     *
+     * @param ex The Exception to handle.
+     */
+    protected void handleException(Exception ex) {
+        StringBuilder details = new StringBuilder();
+        Throwable causes = ex;
+        while (causes.getCause() != null) {
+            details.append(ex.getMessage());
+            details.append("    Caused by:");
+            details.append(causes.getCause().getMessage());
+            causes = causes.getCause();
+        }
+        Messages.create(ex.getMessage()).detail(details.toString()).error().add("errors");
+    }
+
+}
