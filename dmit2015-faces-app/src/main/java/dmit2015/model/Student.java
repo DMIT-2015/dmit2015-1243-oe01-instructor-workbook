4c6362cc8551f2828daa64f77ab39e04b92f891b
Sam Wu <swu@nait.ca>
2025-05-23 20:39:39 -0600

    Added Student model class and micproprofile-config.properties file.

---
+++ dmit2015-faces-app/src/main/java/dmit2015/model/Student.java
@@ -0,0 +1,14 @@
+package dmit2015.model;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private String id;
+
+    private String firstName;
+    private String lastName;
+    private String courseSection;
+
+}
