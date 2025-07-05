955c18c878782dad9b4b7c03f13189c2dd8c59b2
Sam Wu <swu@nait.ca>
2025-07-04 20:53:57 -0600

    Added REST CLient project

--- dmit2015-restapi/src/main/java/dmit2015/dmit201restapi/HelloApplication.java
+++
@@ -1,9 +0,0 @@
-package dmit2015.dmit201restapi;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("/restapi")
-public class HelloApplication extends Application {
-
-}
---
+++ dmit2015-restclient/src/main/java/dmit2015/restclient/Student.java
@@ -0,0 +1,11 @@
+package dmit2015.restclient;
+
+import lombok.Data;
+
+@Data
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+}
