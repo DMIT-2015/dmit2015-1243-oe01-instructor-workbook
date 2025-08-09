33bf6c78485dccf5376690e39aa907f36a416d36
Sam Wu <swu@nait.ca>
2025-08-08 18:00:32 -0600

    added etl-process-dwpubssales project

---
+++ dmit2015-etl-process-dwpubssales/src/main/java/pubs/etlprocessdwpubssales/HelloApplication.java
@@ -0,0 +1,9 @@
+package pubs.etlprocessdwpubssales;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/restapi")
+public class HelloApplication extends Application {
+
+}
